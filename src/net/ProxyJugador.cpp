4f79d04721fcf64b23e97dc3b1923f595548fea8
soyyo5159 <soyyo5159@hotmail.com>
2016-05-16 14:45:58 -0300

    Esto "cierra" el circuito

--- src/net/ProxyJugador.cpp
+++ src/net/ProxyJugador.cpp
@@ -1,11 +1,23 @@
 #include "ProxyJugador.h"
 #include <iostream>
-ProxyJugador::ProxyJugador(Megaman* con, VentanaJuego& ventana):controlado(con){
+#include "../graficos/VentanaJuego.h"
+#include "ServerProxy.h"
+ProxyJugador(Megaman* controlado, VentanaJuego& ventana, ServerProxy& serv):
+					controlado(con),
+					server_proxy(serv){
 	ventana.getWindow().signal_key_press_event().connect(
 			sigc::mem_fun(*this, &ProxyJugador::detectarPresionTecla));
 }
 bool ProxyJugador::detectarPresionTecla(GdkEventKey* evento){
 	controlado->saltar();
 	std::cout<<"se apreto una tecla"<<std::endl;
-	return true;
+	server_proxy.enviarEvento(CODIGO_EVENTO_SALTO);
+	return false;
+}
+ProxyJugador::ProxyJugador(Megaman* controlado): controlado(con){}
+
+void recibirMensajeDeCliente(CodigoEvento evento){
+	if(evento==CODIGO_EVENTO_SALTO){
+		controlado->saltar;
+	}
 }
--- src/net/ProxyJugador.h
+++ src/net/ProxyJugador.h
@@ -1,3 +1,5 @@
+#ifndef PROXYJUGADOR
+#define PROXYJUGADOR
 /**
 Esta clase reresenta el proxy de cada jugador.
 Los playerProxy escuchan del teclado o de un Socket, 
@@ -5,13 +7,37 @@ y actualizan al Megaman* que se les asigne.
 De escuchar del teclado, envían una señal al servidor.
 En e3l futuro se separarán ambas funciones en 2 clases.
 **/
-#include <gtkmm.h>
-#include "../graficos/VentanaJuego.h"
+
+//#include "../graficos/VentanaJuego.h"
+class VentanaJuego;
+class ServerProxy;
 #include "../mundo/Megaman.h"
+class ServerProxy;
+#include <gdk/gdk.h>
+
+typedef char CodigoEvento;
+#define CODIGO_EVENTO_SALTO 's'
+
 class ProxyJugador{
 	private:
 	Megaman* controlado;
+	ServerProxy& server_proxy;
 	public:
-	ProxyJugador(Megaman* controlado, VentanaJuego& ventana);
+	/**
+	 * Usado del lado del cliente
+	 * */
+	ProxyJugador(Megaman* controlado, VentanaJuego& ventana, ServerProxy& server_proxy);
+	/**
+	 * Usado por el server
+	 * */
+	ProxyJugador(Megaman* controlado);
+	/**
+	 * Esta se uso del lado de cliente para detectar el teclado
+	 * */
 	bool detectarPresionTecla(GdkEventKey* evento);
+	/**
+	 * Esta se usa del lado del servidor para recibir los keystrokes remotos...
+	 * */
+	void recibirMensajeDeCliente(CodigoEvento evento);
 };
+#endif
